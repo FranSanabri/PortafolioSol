ba2bf63540a49183e1053312e1346717deb53446
FranSanabri <death.the.system@gmail.com>
2023-05-31 16:02:16 -0300

    Commit

--- src/components/About Me/AboutMe.jsx
+++ src/components/About Me/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
         <div className="container" id="aboutme">
           <h1 className="About">Acerca de mi</h1>
           <p className="name">Soy estudiante de Licenciatura en Psicologia en la Universidad Argentina Jonh F Kennedy, con el 70% de las materias aprobodas. Me encataria insertarme en el mundo laboral ocupando un lugar donde pueda aportar valor, aprender, desarrollarme, y ayudar al resto de las personas con las que trabajo.</p>
-          <img src="https://i.pinimg.com/750x/19/01/f7/1901f72a3961e64a8b71379bcd24cc56.jpg" className="foto"></img>
+          <img src="https://i.pinimg.com/originals/d7/f0/44/d7f04426f1116d51976ce844369b1963.jpg" className="foto"></img>
         </div>
     
     );
